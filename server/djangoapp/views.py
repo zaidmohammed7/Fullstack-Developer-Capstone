c3f47457f3b99789a3819c7a5e5caaf52350ab45
Zaid Khan Mohammed <zaidkhanmd27@gmail.com>
2025-01-17 17:29:19 -0500

    Reformatted files

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
 # from django.contrib import messages
-from datetime import datetime
+# from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -12,7 +12,8 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments, post_review
+from .restapis import get_request, analyze_review_sentiments
+# from .restapis import post_review
 
 
 # Get an instance of a logger
@@ -122,7 +123,7 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
 def add_review(request):
     if request.user.is_anonymous is False:
-        data = json.loads(request.body)
+        # data = json.loads(request.body)
         try:
             # response = post_review(data)
             return JsonResponse({"status": 200})
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,12 +31,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
-    ("https://zaidkhanmd27-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01"
-    ".proxy.cognitiveclass.ai"),
+    (
+        "https://zaidkhanmd27-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01"
+        ".proxy.cognitiveclass.ai"
+    ),
 ]
 CSRF_TRUSTED_ORIGINS = [
-    ("https://zaidkhanmd27-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01"
-    ".proxy.cognitiveclass.ai")
+    (
+        "https://zaidkhanmd27-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01"
+        ".proxy.cognitiveclass.ai"
+    )
 ]
 
 REST_FRAMEWORK = {
@@ -101,20 +105,28 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": ("django.contrib.auth.password_validation"
-        ".UserAttributeSimilarityValidator"),
+        "NAME": (
+            "django.contrib.auth.password_validation"
+            ".UserAttributeSimilarityValidator"
+        ),
     },
     {
-        "NAME": ("django.contrib.auth.password_validation"
-        ".MinimumLengthValidator"),
+        "NAME": (
+            "django.contrib.auth.password_validation"
+            ".MinimumLengthValidator"
+        ),
     },
     {
-        "NAME": ("django.contrib.auth.password_validation"
-        ".CommonPasswordValidator"),
+        "NAME": (
+            "django.contrib.auth.password_validation"
+            ".CommonPasswordValidator"
+        ),
     },
     {
-        "NAME": ("django.contrib.auth.password_validation"
-        ".NumericPasswordValidator"),
+        "NAME": (
+            "django.contrib.auth.password_validation"
+        ".NumericPasswordValidator"
+        ),
     },
 ]
 
